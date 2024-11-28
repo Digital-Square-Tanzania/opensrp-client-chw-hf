e6403cb739ef6273597e8ae7642d4bf70323a6e1
cozej4 <ilakozejumanne@gmail.com>
2024-11-28 15:21:30 +0300

    :hammer: fixed building issues

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/activity/AllClientsMemberProfileActivity.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/activity/AllClientsMemberProfileActivity.java
@@ -402,16 +402,6 @@ public class AllClientsMemberProfileActivity extends CoreAllClientsMemberProfile
     }
 
     @Override
-    protected void startCancerPreventiveServicesRegistration() {
-
-    }
-
-    @Override
-    protected void startAsrhRegistration() {
-
-    }
-
-    @Override
     protected void setIndependentClient(boolean isIndependentClient) {
         super.isIndependent = isIndependentClient;
     }
--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/activity/FamilyOtherMemberProfileActivity.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/activity/FamilyOtherMemberProfileActivity.java
@@ -233,16 +233,6 @@ public class FamilyOtherMemberProfileActivity extends CoreFamilyOtherMemberProfi
     }
 
     @Override
-    protected void startCancerPreventiveServicesRegistration() {
-
-    }
-
-    @Override
-    protected void startAsrhRegistration() {
-
-    }
-
-    @Override
     protected void setIndependentClient(boolean b) {
         this.isIndependent = false;
     }
