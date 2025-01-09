e822e8625d62091bebd8bee30579aaf4696e52a2
Ilakoze Jumanne <ilakozejumanne@gmail.com>
2025-01-09 11:25:18 +0300

    :hammer: Fixed Reported KVP Issues (#70)

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/interactor/LoginInteractor.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/interactor/LoginInteractor.java
@@ -88,7 +88,7 @@ public class LoginInteractor extends BaseLoginInteractor implements BaseLoginCon
         StockUsageReportJob.scheduleJobImmediately(StockUsageReportJob.TAG);
         SyncLocationsByTeamIdsJob.scheduleJobImmediately(SyncLocationsByTeamIdsJob.TAG);
         SyncLocationsByLevelAndTagsServiceJob.scheduleJobImmediately(SyncLocationsByLevelAndTagsServiceJob.TAG);
-        ChwIndicatorGeneratingJob.scheduleJobImmediately(ChwIndicatorGeneratingJob.TAG);
+//        ChwIndicatorGeneratingJob.scheduleJobImmediately(ChwIndicatorGeneratingJob.TAG);
         ProcessVisitsServiceJob.scheduleJobImmediately(ProcessVisitsServiceJob.TAG);
         PncCloseDateServiceJob.scheduleJobImmediately(PncCloseDateServiceJob.TAG);
         PullUniqueLabTestSampleTrackingIdsServiceJob.scheduleJobImmediately(PullUniqueLabTestSampleTrackingIdsServiceJob.TAG);
