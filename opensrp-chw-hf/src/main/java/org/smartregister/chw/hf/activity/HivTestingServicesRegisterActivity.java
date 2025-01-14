9d9c53b44a9e098996352ee3d79fdc36f12bbff3
Milton <miltonisaiah@gmail.com>
2025-01-14 18:38:47 +0300

    global variables passed to form

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/activity/HivTestingServicesRegisterActivity.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/activity/HivTestingServicesRegisterActivity.java
@@ -1,25 +1,49 @@
 package org.smartregister.chw.hf.activity;
 
+import static org.smartregister.client.utils.constants.JsonFormConstants.JSON_FORM_KEY.GLOBAL;
+
 import android.app.Activity;
 import android.content.Intent;
 
 import androidx.annotation.MenuRes;
 import androidx.fragment.app.Fragment;
 
+import com.vijay.jsonwizard.constants.JsonFormConstants;
 import com.vijay.jsonwizard.domain.Form;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.smartregister.chw.core.utils.FormUtils;
 import org.smartregister.chw.hts.util.Constants;
 import org.smartregister.chw.core.activity.CoreHtsRegisterActivity;
 import org.smartregister.chw.hf.R;
 import org.smartregister.chw.hf.fragment.HivTestingServicesRegisterFragment;
+import org.smartregister.family.util.JsonFormUtils;
 import org.smartregister.view.fragment.BaseRegisterFragment;
+
+import timber.log.Timber;
 
 public class HivTestingServicesRegisterActivity extends CoreHtsRegisterActivity {
 
-    public static void startRegistration(Activity activity, String baseEntityId) {
+    public static void startRegistration(Activity activity, String baseEntityId, int clientAge, String sex) {
         Intent intent = new Intent(activity, HivTestingServicesRegisterActivity.class);
         intent.putExtra(Constants.ACTIVITY_PAYLOAD.BASE_ENTITY_ID, baseEntityId);
-        intent.putExtra(Constants.ACTIVITY_PAYLOAD.HTS_FORM_NAME, Constants.FORMS.HTS_SCREENING);
+
+        if(clientAge >=15){
+            //Open 15 and above form
+            intent.putExtra(Constants.ACTIVITY_PAYLOAD.HTS_FORM_NAME, Constants.FORMS.HTS_SCREENING_15_AND_ABOVE);
+            intent.putExtra(Constants.ACTIVITY_PAYLOAD.SEX, sex);
+        }
+
+        if(clientAge >=10 && clientAge <=14){
+            //Open form for clients aged >=10 and less than or equal to 14
+            intent.putExtra(Constants.ACTIVITY_PAYLOAD.HTS_FORM_NAME, Constants.FORMS.HTS_SCREENING_10_TO_14);
+        }
+
+        if(clientAge >= 2 && clientAge <=9){
+            //Open form for clients aged >=2 and less than or equal to 9
+            intent.putExtra(Constants.ACTIVITY_PAYLOAD.HTS_FORM_NAME, Constants.FORMS.HTS_SCREENING_2_TO_9);
+        }
 
         activity.startActivity(intent);
     }
@@ -50,6 +74,23 @@ public class HivTestingServicesRegisterActivity extends CoreHtsRegisterActivity 
     @MenuRes
     public int getMenuResource() {
         return R.menu.bottom_nav_cecap_menu;
+    }
+
+    @Override
+    public void startFormActivity(JSONObject jsonForm) {
+        if(FORM_NAME.equals(Constants.FORMS.HTS_SCREENING_15_AND_ABOVE)){
+            try {
+                jsonForm.getJSONObject(GLOBAL).put("sex",sex);
+            } catch (JSONException e) {
+                Timber.e(e);
+            }
+        }
+
+        Intent intent = FormUtils.getStartFormActivity(jsonForm, null, this);
+        if (getFormConfig() != null) {
+            intent.putExtra(JsonFormConstants.JSON_FORM_KEY.FORM, getFormConfig());
+        }
+        startActivityForResult(intent, JsonFormUtils.REQUEST_CODE_GET_JSON);
     }
 
 }
