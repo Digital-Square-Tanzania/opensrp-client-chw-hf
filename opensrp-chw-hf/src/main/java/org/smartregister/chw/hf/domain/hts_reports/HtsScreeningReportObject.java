718b0758532e8c50153c07549861ad0a00a2ad0f
tbwahacker <twaloadam@gmail.com>
2025-01-17 16:33:54 +0300

    initial stage for hts reports

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/domain/hts_reports/HtsScreeningReportObject.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/domain/hts_reports/HtsScreeningReportObject.java
@@ -7,24 +7,13 @@ import org.smartregister.chw.hf.domain.ReportObject;
 
 import java.util.Date;
 
+import timber.log.Timber;
+
 public class HtsScreeningReportObject extends ReportObject {
 
-
-    private final String[] kvpQuestionsGroups = new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14-a",
-            "14-b-i", "14-b-ii", "14-b-iii", "14-b-iv", "14-b-v", "14-b-vi",
-            "15", "16", "18", "19", "20", "21", "22",
-            "17-a", "17-b", "17-c", "17-d"
-    };
-    private final String[] kvpAgeGroups = new String[]{
-            "<10", "10-14", "15-19", "20-24", "25-29", "30-34", "35-39", "40-44", "45-49", ">50"
-    };
-    private final String[] kvpGroups = new String[]{
-            "pwid", "pwud", "fsw", "msm", "agyw", "mobile_population", "serodiscordant_couple", "other_vulnerable_population"
-    };
-    private final String[] kvpGenderGroups = new String[]{
-            "ME", "KE"
-    };
-
+    private final String[] htsQuestionsGroups = new String[]{"1", "2", "3", "4", "5", "6"};
+    private final String[] htsAgeGroups = new String[]{"2-9", "10-14", ">=15"};
+    private final String[] htsGenderGroups = new String[]{"ME", "KE"};
     private final Date reportDate;
     private JSONObject jsonObject;
 
@@ -36,13 +25,12 @@ public class HtsScreeningReportObject extends ReportObject {
     @Override
     public JSONObject getIndicatorData() throws JSONException {
         jsonObject = new JSONObject();
-        for (String questionGroup : kvpQuestionsGroups) {   //rows
-            for (String ageGroup : kvpAgeGroups) {  //columns
-                for (String kvpGroup : kvpGroups) {
-                    for (String genderGroup : kvpGenderGroups) {  //concstenate rows columns and gendergroup
-                        jsonObject.put("kvp" + "-" + questionGroup + "-" + ageGroup + "-" + kvpGroup + "-" + genderGroup,
-                                ReportDao.getReportPerIndicatorCode("kvp" + "-" + questionGroup + "-" + ageGroup + "-" + kvpGroup + "-" + genderGroup, reportDate));
-                    }
+        for (String questionGroup : htsQuestionsGroups) {   //rows
+            for (String ageGroup : htsAgeGroups) {  //columns
+                for (String genderGroup : htsGenderGroups) {  //concstenate rows columns and gendergroup
+                    Timber.tag("anganaili").d(""+ReportDao.getReportPerIndicatorCode("hts" + "-" + questionGroup + "-" + ageGroup + "-" + genderGroup, reportDate));
+                    jsonObject.put("hts" + "-" + questionGroup + "-" + ageGroup + "-" + genderGroup,
+                            ReportDao.getReportPerIndicatorCode("hts" + "-" + questionGroup + "-" + ageGroup + "-" + genderGroup, reportDate));
                 }
             }
         }
@@ -53,15 +41,15 @@ public class HtsScreeningReportObject extends ReportObject {
         return jsonObject;
     }
 
-    private int getTotalPerEachIndicator(String question, String kvpgroup) throws JSONException {
+    private int getTotalPerEachIndicator(String question) throws JSONException {
         int totalOfGenderGiven = 0;
         int returnedValue = 0;
-        for (String age : kvpAgeGroups) {
-            totalOfGenderGiven += (ReportDao.getReportPerIndicatorCode("kvp" + "-"
-                    + question + "-" + age + "-" + kvpgroup + "-" + "ME", reportDate)
-                    + ReportDao.getReportPerIndicatorCode("kvp" + "-"
-                    + question + "-" + age + "-" + kvpgroup + "-" + "KE", reportDate));
-            jsonObject.put("kvp" + "-" + question + "-" + kvpgroup + "-jumla-both-ME-KE", totalOfGenderGiven);  //display the total for both gender
+        for (String age : htsAgeGroups) {
+            totalOfGenderGiven += (ReportDao.getReportPerIndicatorCode("hts" + "-"
+                    + question + "-" + age + "-" + "ME", reportDate)
+                    + ReportDao.getReportPerIndicatorCode("hts" + "-"
+                    + question + "-" + age + "-" + "KE", reportDate));
+            jsonObject.put("hts" + "-" + question + "-" + "jumla-both-ME-KE", totalOfGenderGiven);  //display the total for both gender
             returnedValue = totalOfGenderGiven;
         }
         return returnedValue;
@@ -69,13 +57,11 @@ public class HtsScreeningReportObject extends ReportObject {
 
 
     private void funcGetTotal() throws JSONException {
-        int totalofthewholekvpgroup = 0;
-        for (String question : kvpQuestionsGroups) {
-            for (String kvpGroup : kvpGroups) {
-                totalofthewholekvpgroup += getTotalPerEachIndicator(question, kvpGroup);
-                jsonObject.put("kvp" + "-" + question + "-jumla-kuu", totalofthewholekvpgroup); //total for all kvp groups
-            }
-            totalofthewholekvpgroup = 0;
+        int totalofthewholehtsgroup = 0;
+        for (String question : htsQuestionsGroups) {
+            totalofthewholehtsgroup += getTotalPerEachIndicator(question);
+            jsonObject.put("hts" + "-" + question + "-jumla-kuu", totalofthewholehtsgroup); //total for all hts groups
+            totalofthewholehtsgroup = 0;
         }
     }
 
--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/repository/HfChwRepository.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/repository/HfChwRepository.java
@@ -154,12 +154,17 @@ public class HfChwRepository extends CoreChwRepository {
                 String vmmcOutreachIndicatorConfigFile = "config/vmmc-outreach-report.yml";
                 String kvpTestingIndicatorConfigFile = "config/kvp-monthly-report.yml";
                 String ltfuIndicatorConfigFile = "config/community-ltfu-summary.yml";
+                String htsMonthlyIndicatorConfigFile = "config/hts-monthly-report.yml";
+                String htsScreeningIndicatorConfigFile = "config/hts-screening-report.yml";
+                String tbMonthlyIndicatorConfigFile = "config/tb-monthly-report.yml";
+                String tbScreeningIndicatorConfigFile = "config/tb-screening-report.yml";
 
                 for (String configFile : Collections.unmodifiableList(
                         Arrays.asList(indicatorsConfigFile, ancIndicatorConfigFile,
                                 pmtctIndicatorConfigFile, pncIndicatorConfigFile,
                                 cbhsReportingIndicatorConfigFile, ldReportingIndicatorConfigFile,
-                                motherChampionReportingIndicatorConfigFile, vmmcIndicatorConfigFile, vmmcStaticIndicatorConfigFile, vmmcOutreachIndicatorConfigFile, selfTestingIndicatorConfigFile, kvpTestingIndicatorConfigFile, ltfuIndicatorConfigFile))) {
+                                motherChampionReportingIndicatorConfigFile, vmmcIndicatorConfigFile, vmmcStaticIndicatorConfigFile, vmmcOutreachIndicatorConfigFile, selfTestingIndicatorConfigFile,
+                                kvpTestingIndicatorConfigFile, ltfuIndicatorConfigFile, htsMonthlyIndicatorConfigFile, htsScreeningIndicatorConfigFile, tbMonthlyIndicatorConfigFile, tbScreeningIndicatorConfigFile))) {
                     reportingLibraryInstance.readConfigFile(configFile, db);
                 }
 
@@ -506,13 +511,19 @@ public class HfChwRepository extends CoreChwRepository {
                 String cecapIndicatorConfigFile = "config/cecap-reporting-indicator-definitions.yml";
                 String cecapOtherReportsIndicatorConfigFile = "config/cecap-other-reporting-indicator-definitions.yml";
                 String asrhReportsIndicatorConfigFile = "config/asrh-reporting-indicator-definitions.yml";
-
+                String htsMonthlyIndicatorConfigFile = "config/hts-monthly-report.yml";
+                String htsScreeningIndicatorConfigFile = "config/hts-screening-report.yml";
+                String tbMonthlyIndicatorConfigFile = "config/tb-monthly-report.yml";
+                String tbScreeningIndicatorConfigFile = "config/tb-screening-report.yml";
 
                 for (String configFile : Collections.unmodifiableList(
                         Arrays.asList(indicatorsConfigFile, ancIndicatorConfigFile,
                                 pmtctIndicatorConfigFile, pncIndicatorConfigFile,
                                 cbhsReportingIndicatorConfigFile, ldReportingIndicatorConfigFile,
-                                motherChampionReportingIndicatorConfigFile, selfTestingIndicatorConfigFile, kvpTestingIndicatorConfigFile, ltfuIndicatorConfigFile, vmmcIndicatorConfigFile, vmmcStaticIndicatorConfigFile, vmmcOutreachIndicatorConfigFile, fpIndicatorConfigFile, cecapIndicatorConfigFile, cecapOtherReportsIndicatorConfigFile,asrhReportsIndicatorConfigFile))) {
+                                motherChampionReportingIndicatorConfigFile, selfTestingIndicatorConfigFile, kvpTestingIndicatorConfigFile,
+                                ltfuIndicatorConfigFile, vmmcIndicatorConfigFile, vmmcStaticIndicatorConfigFile, vmmcOutreachIndicatorConfigFile,
+                                fpIndicatorConfigFile, cecapIndicatorConfigFile, cecapOtherReportsIndicatorConfigFile,asrhReportsIndicatorConfigFile,
+                                htsMonthlyIndicatorConfigFile, htsScreeningIndicatorConfigFile, tbMonthlyIndicatorConfigFile, tbScreeningIndicatorConfigFile))) {
                     reportingLibraryInstance.readConfigFile(configFile, db);
                 }
 
