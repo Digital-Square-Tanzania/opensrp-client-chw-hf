ebd4b20074111b188183dc10a02b680a00242468
cozej4 <ilakozejumanne@gmail.com>
2025-07-07 20:10:41 +0300

    :hammer: Updated FP db migrations and updated db version

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/repository/HfChwRepository.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/repository/HfChwRepository.java
@@ -497,6 +497,15 @@ public class HfChwRepository extends CoreChwRepository {
         }
     }
 
+    private static void upgradeToVersion29(SQLiteDatabase db) {
+        try {
+            db.execSQL("ALTER TABLE ec_family_planning ADD COLUMN ctc_number TEXT NULL;");
+        } catch (Exception e) {
+            Timber.e(e);
+        }
+    }
+
+
     private static void upgradeToVersion10ForBaSouth(SQLiteDatabase db) {
         try {
             db.execSQL("ALTER TABLE ec_family_member ADD COLUMN reasons_for_registration TEXT NULL;");
@@ -646,6 +655,9 @@ public class HfChwRepository extends CoreChwRepository {
                     break;
                 case 28:
                     upgradeToVersion28(db);
+                    break;
+                case 29:
+                    upgradeToVersion29(db);
                     break;
                 default:
                     break;
