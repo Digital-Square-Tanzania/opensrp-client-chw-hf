e440a9f5453de82de8345b69405392a934f43574
cozej4 <ilakozejumanne@gmail.com>
2025-12-12 17:29:02 +0300

    🐛 Fix HTS child visit detail prefix mapping

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/interactor/HtsMedicalHistoryInteractor.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/interactor/HtsMedicalHistoryInteractor.java
@@ -4,7 +4,10 @@ import static org.smartregister.chw.anc.util.VisitUtils.getChildVisits;
 import static org.smartregister.chw.anc.util.VisitUtils.getVisitDetailsOnly;
 import static org.smartregister.chw.anc.util.VisitUtils.getVisitGroups;
 import static org.smartregister.chw.anc.util.VisitUtils.getVisitsOnly;
+import static org.smartregister.chw.hts.util.Constants.EVENT_TYPE.HTS_FIRST_HIV_TEST;
+import static org.smartregister.chw.hts.util.Constants.EVENT_TYPE.HTS_SECOND_HIV_TEST;
 import static org.smartregister.chw.hts.util.Constants.EVENT_TYPE.HTS_SERVICES;
+import static org.smartregister.chw.hts.util.Constants.EVENT_TYPE.HTS_UNIGOLD_HIV_TEST;
 
 import android.content.Context;
 
@@ -69,8 +72,16 @@ public class HtsMedicalHistoryInteractor extends CoreBaseAncMedicalHistoryIntera
                 List<Visit> childVisits = getChildVisits(visit.getVisitId());
                 for (Visit childVisit : childVisits) {
                     Map<String, List<VisitDetail>> mVisitDetails = childVisit.getVisitDetails();
+                    if (mVisitDetails == null) {
+                        continue;
+                    }
+                    Map<String, String> prefixedKeys = getPrefixedChildVisitDetailKeys(childVisit.getVisitType());
                     for (Map.Entry<String, List<VisitDetail>> entry : mVisitDetails.entrySet()) {
                         visitDetails.computeIfAbsent(entry.getKey(), key -> new ArrayList<>()).addAll(entry.getValue());
+                        String prefixedKey = prefixedKeys.get(entry.getKey());
+                        if (prefixedKey != null && !prefixedKey.equals(entry.getKey())) {
+                            visitDetails.computeIfAbsent(prefixedKey, key -> new ArrayList<>()).addAll(entry.getValue());
+                        }
                     }
                 }
 
@@ -81,5 +92,28 @@ public class HtsMedicalHistoryInteractor extends CoreBaseAncMedicalHistoryIntera
         };
 
         appExecutors.diskIO().execute(runnable);
+    }
+
+    private Map<String, String> getPrefixedChildVisitDetailKeys(String visitType) {
+        Map<String, String> prefixedKeys = new HashMap<>();
+        if (visitType == null) {
+            return prefixedKeys;
+        }
+        if (visitType.startsWith(HTS_FIRST_HIV_TEST)) {
+            prefixedKeys.put("type_of_test_kit_used", "hts_type_of_test_used");
+            prefixedKeys.put("test_kit_batch_number", "hts_kit_batch_number");
+            prefixedKeys.put("test_kit_expire_date", "hts_kit_expire_date");
+            prefixedKeys.put("test_result", "hts_first_hiv_test_result");
+            prefixedKeys.put("syphilis_test_results", "syphilis_test_results");
+        } else if (visitType.startsWith(HTS_SECOND_HIV_TEST)) {
+            prefixedKeys.put("test_kit_batch_number", "hts_second_kit_batch_number");
+            prefixedKeys.put("test_kit_expire_date", "hts_second_kit_expire_date");
+            prefixedKeys.put("test_result", "hts_second_hiv_test_result");
+        } else if (visitType.startsWith(HTS_UNIGOLD_HIV_TEST)) {
+            prefixedKeys.put("test_kit_batch_number", "hts_unigold_kit_batch_number");
+            prefixedKeys.put("test_kit_expire_date", "hts_unigold_kit_expire_date");
+            prefixedKeys.put("test_result", "hts_unigold_hiv_test_result");
+        }
+        return prefixedKeys;
     }
 }
