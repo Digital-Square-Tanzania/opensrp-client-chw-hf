621162b6433690cea4b5e2c7f9964f22f2ec0160
cozej4 <ilakozejumanne@gmail.com>
2026-08-18 15:57:31 +0300

    🐛 Link HIVST results to source form submission

--- opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/repository/HfChwRepository.java
+++ opensrp-chw-hf/src/main/java/org/smartregister/chw/hf/repository/HfChwRepository.java
@@ -568,6 +568,15 @@ public class HfChwRepository extends CoreChwRepository {
         }
     }
 
+    private static void upgradeToVersion31(SQLiteDatabase db) {
+        try {
+            String addMissingColumnsQuery = "ALTER TABLE ec_hivst_results ADD COLUMN source_form_submission_id VARCHAR;";
+            db.execSQL(addMissingColumnsQuery);
+        } catch (Exception e) {
+            Timber.e(e, "upgradeToVersion31");
+        }
+    }
+
 
     private static void upgradeToVersion10ForBaSouth(SQLiteDatabase db) {
         try {
@@ -725,6 +734,9 @@ public class HfChwRepository extends CoreChwRepository {
                     upgradeToVersion29(db);
                 case 30:
                     upgradeToVersion30(db);
+                    break;
+                case 31:
+                    upgradeToVersion31(db);
                     break;
                 default:
                     break;
